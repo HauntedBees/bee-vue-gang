788f697a3e8f21675f2b02441ec87bdbd4f0a374
Haunted Bees <fench@hauntedbees.com>
2021-01-20 23:03:48 -0800

    alright

--- src/SpanTooltip/SpanTooltip.vue
+++ src/SpanTooltip/SpanTooltip.vue
@@ -1,16 +1,17 @@
 <template>
-    <v-tooltip bottom>
+    <VTooltip bottom>
         <template v-slot:activator="{ on, attrs }">
             <span v-bind="attrs" v-on="on">
                 <slot></slot>
             </span>
         </template>
         <span>{{tooltip}}</span>
-    </v-tooltip>
+    </VTooltip>
 </template>
 <script lang="ts">
 import { Vue, Component, Prop } from 'vue-property-decorator';
-@Component
+import { VTooltip } from 'vuetify/lib';
+@Component({ components: { VTooltip } })
 export default class SpanTooltip extends Vue {
     @Prop() tooltip?:string;
 }
