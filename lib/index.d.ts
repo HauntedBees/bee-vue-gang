ae5e722486f3b307d44d67d809b10a09e72dee15
Haunted Bees <fench@hauntedbees.com>
2021-01-24 23:56:25 -0800

    oops forgot to include them

--- lib/index.d.ts
+++ lib/index.d.ts
@@ -3,4 +3,6 @@ import Attribution from "./Attribution/Attribution.vue";
 import Emoji from "./Emoji/Emoji.vue";
 import SpanTooltip from "./SpanTooltip/SpanTooltip.vue";
 import ClassDirective from "./ClassDirective/ClassDirective";
-export { ExternalLink, Attribution, Emoji, SpanTooltip, ClassDirective };
+import LoadableButton from "./LoadableButton/LoadableButton.vue";
+import Loader from "./Loader/Loader.vue";
+export { ExternalLink, Attribution, Emoji, SpanTooltip, ClassDirective, LoadableButton, Loader };
